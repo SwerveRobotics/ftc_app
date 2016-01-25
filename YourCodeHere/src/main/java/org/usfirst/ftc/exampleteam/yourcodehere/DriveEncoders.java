7aa9cb62e538e0d2f78474054ce4af368e6b5f7a
Teameureka <rreynolds@teameureka.net>
2016-01-24 20:07:21 -0700

    DriveEncoder
    cleaned up commenting

--- YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/DriveEncoders.java
+++ YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/DriveEncoders.java
@@ -7,31 +7,35 @@ import org.swerverobotics.library.SynchronousOpMode;
 import org.swerverobotics.library.interfaces.Autonomous;
 import org.swerverobotics.library.interfaces.IFunc;
 
-
 /**
- * This simple OpMode illustrates how to drive autonomously a certain distance using encoders.
+ * This is an example OpMode illustrating how to drive autonomously a certain distance using encoders.
  *
  * The OpMode works with both legacy and modern motor controllers. It expects two motors,
- * named "motorLeft" and "motorRight".
+ * named "motorLeft" and "motorRight". And can be used with both HiTechnic and Neverest motors
+ * Note: the encoder constant must be determined for your robot configuration (wheel size & gearing)
+ *
  */
 
-@Autonomous(name="DriveEncoders")
+@Autonomous(name="DriveEncoders") // adds to registry file for name to appear on driver station
 // @Disabled
         public class DriveEncoders extends SynchronousOpMode {
     //----------------------------------------------------------------------------------------------
     // Declare Hardware
     //----------------------------------------------------------------------------------------------
 
-    // The number of encoder ticks per motor shaft revolution.
+    /** The number of encoder ticks per motor shaft revolution.
     // 1440 HiTechnic motors.
     // 1120 Andy Mark Neverest motors
-    // http://www.cougarrobot.com/index.php?option=com_content&view=article&id=331%3Aandymark-neverest-motor-notes&catid=92%3Aftc-hardware&Itemid=140
+    // documentation found at here http://www.cougarrobot.com/index.php?option=com_content&view=article&id=331%3Aandymark-neverest-motor-notes&catid=92%3Aftc-hardware&Itemid=140
+    */
 
     DcMotor motorRight;
     DcMotor motorLeft;
-    //constant calculations: determine the Counts
+
     int encRotations = 1120;
-    int constant = 1234;
+    int constant = 1234;    /** to determine encoder constant divide encRotations by the distance travelled in one full rotation
+                            *   this will provide the encoder counts equal to 1 inch of travel.
+ *                          */
 
     //----------------------------------------------------------------------------------------------
     // Main loop
@@ -40,7 +44,7 @@ import org.swerverobotics.library.interfaces.IFunc;
     @Override
     protected void main() throws InterruptedException {
         //-------------------------------------
-        // Initialize Hardware
+        // Initialize Hardware.  Must match names given in configuration file on RC app
         //-------------------------------------
 
         this.motorLeft = this.hardwareMap.dcMotor.get("motorLeft");
@@ -50,7 +54,7 @@ import org.swerverobotics.library.interfaces.IFunc;
 
         waitForStart();
 
-    //Autonomous Code goes here....
+        //Autonomous Code goes here....
         DriveEncoderDistance(0.5,10);
 
     }//Main
@@ -58,18 +62,20 @@ import org.swerverobotics.library.interfaces.IFunc;
     //----------------------------------------------------------------------------------------------
     // Utility - SubMethods
     //----------------------------------------------------------------------------------------------
+    
 
-    public void DriveEncoderDistance(double power, int distance) throws InterruptedException{
+    public void DriveEncoderDistance(double power, int distance) throws InterruptedException{    //method to drive set power and distance using encoders
+
         //reset encoders
         motorLeft.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         motorRight.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         // waits for encoders to read zero
         while(motorLeft.getCurrentPosition() != 0 ||
                 motorRight.getCurrentPosition() != 0) {
-            //empty loop to wait until zero is reached
+            //empty loop waits to allow encoders to reach zero before moving on
         }
 
-        //calculate encoder clicks
+        //calculate encoder clicks. Takes desired distance of inches and multiplies by constant
         int COUNTS = distance * constant;
 
         //Set Target
@@ -79,7 +85,7 @@ import org.swerverobotics.library.interfaces.IFunc;
         //Set Power
         DriveForward(power);
 
-        // Set them a-going
+        // turns motors on
         this.motorLeft.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
         this.motorRight.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
 
@@ -93,14 +99,12 @@ import org.swerverobotics.library.interfaces.IFunc;
         motorLeft.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         motorRight.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
 
-    }//DriveEncoder
-
-    public void DriveForward(double power) {
+    }//DriveEncoderDistance
+    public void DriveForward(double power) {    //sets both motors to desired power
         motorLeft.setPower(power);
         motorRight.setPower(power);
     }
-
-    public void StopDriving() {
+    public void StopDriving() {     //sets both motors to zero power
         DriveForward(0);
     }
 
